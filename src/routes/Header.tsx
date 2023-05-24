4af61566f932923b212fe36d74526bf2b2cf816a
kimminkum <h24breaker@naver.com>
2023-05-24 14:28:21 +0900

    goodus responsive

--- src/routes/Header.tsx
+++ src/routes/Header.tsx
@@ -1,17 +1,26 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import "./Header.css";
+import imgLogo from "../image/logo.png";
 
 function Header() {
   return (
-    <div>
-      <div className="topbar">
+    <div className="mb">
+      <div className="topbar flex_sb">
+        {/* bar */}
+        <div>
+          <a href="">happy</a>
+        </div>
+
         {/* logo */}
         <Link to="/">
-          <div></div>
+          <div>
+            <img src={imgLogo} alt="" />
+          </div>
         </Link>
 
-        {/* bar */}
-        <div></div>
+        {/*  */}
+        <div>d</div>
       </div>
     </div>
   );
