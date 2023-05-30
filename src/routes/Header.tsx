2e006826d24f3c3be5464ab80cc73583ef52f158
kimminkum <h24breaker@naver.com>
2023-05-30 11:35:46 +0900

    icons and respon challenge

--- src/routes/Header.tsx
+++ src/routes/Header.tsx
@@ -1,26 +1,30 @@
-import React from "react";
+import React, { ReactNode } from "react";
 import { Link } from "react-router-dom";
 import "./Header.css";
 import imgLogo from "../image/logo.png";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faBars } from "@fortawesome/free-solid-svg-icons";
+import { useMediaQuery } from "react-responsive";
 
 function Header() {
   return (
-    <div className="mb">
+    <div>
       <div className="topbar flex_sb">
-        {/* bar */}
-        <div>
-          <a href="">happy</a>
-        </div>
-
-        {/* logo */}
+        {/* Logo */}
         <Link to="/">
           <div>
             <img src={imgLogo} alt="" />
           </div>
         </Link>
 
-        {/*  */}
-        <div>d</div>
+        <div>
+          {/* pc */}
+          <div className="flex_end"></div>
+          {/* mobile */}
+          <div>
+            <FontAwesomeIcon icon={faBars} />
+          </div>
+        </div>
       </div>
     </div>
   );
